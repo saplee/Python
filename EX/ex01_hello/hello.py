450bf80adbc490b34343aa25ce16638b91c59dfd
Sander Pleesi <saplee@ttu.ee>
2021-09-03 18:10:06 +0300

    v4

--- EX/ex01_hello/hello.py
+++ EX/ex01_hello/hello.py
@@ -1,15 +1,15 @@
 
-# ask for a name
+# ask for a name.
 name = input("What is your name?")
-# ask for first random number
+# ask for first random number.
 num1 = int(input(f"Hello, {name}! Enter a random number:"))
-# ask for second random number
+# ask for second random number.
 num2 = int(input("Great! Now enter a second random number:"))
 sum1 = num1+num2
-# print out sum
+# print out sum.
 print(f"{num1} + {num2} is {sum1}")
 
-#ask 
+
 some_color = input("Enter some color")
 noun = input("Enter some plural noun")
 verb = input("Enter some verb")
