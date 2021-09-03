cdb427dca746875e8daf8bd7989f9fbd8fd4fa09
Sander Pleesi <saplee@ttu.ee>
2021-09-03 19:01:05 +0300

    v4

--- EX/ex01_hello/hello.py
+++ EX/ex01_hello/hello.py
@@ -1,11 +1,10 @@
-
 # ask for a name.
 name = input("What is your name?")
 # ask for first random number.
 num1 = int(input(f"Hello, {name}! Enter a random number:"))
 # ask for second random number.
 num2 = int(input("Great! Now enter a second random number:"))
-sum1 = num1+num2
+sum1 = num1 + num2
 # print out sum.
 print(f"{num1} + {num2} is {sum1}")
 
