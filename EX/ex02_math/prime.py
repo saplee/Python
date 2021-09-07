763563b137050cf361a837b9dce0f7550d962ed8
Sander Pleesi <saplee@ttu.ee>
2021-09-07 15:11:45 +0300

    v4

--- EX/ex02_math/prime.py
+++ EX/ex02_math/prime.py
@@ -2,10 +2,12 @@ def is_prime_number(number):
     for num in range(2, number):
         if number % num == 0:
             return False
+    if number <= 1:
+        return False
     return True
 
 
-print(is_prime_number(2))
+print(is_prime_number(1))
 print(is_prime_number(89))
 print(is_prime_number(23))
 print(is_prime_number(4))
