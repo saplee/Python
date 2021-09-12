11fd59497df8aba25dbac4143b4d1a609d0e8509
Sander Pleesi <saplee@ttu.ee>
2021-09-13 01:53:27 +0300

    v4

--- EX/ex03_idcode/idcode.py
+++ EX/ex03_idcode/idcode.py
@@ -1,16 +1,18 @@
 import re
+
+
 def find_id_code(string):
-    numbers=(re.findall('\d', string))
-    if len(numbers)<11:
+    numbers = (re.findall('\d', string))
+    id_code = "".join(numbers)
+    if len(id_code) < 11:
         return "Not enough numbers!"
-    if len(numbers)>11:
+    if len(id_code) > 11:
         return "Too many numbers!"
     else:
-        return numbers
+        return id_code
 
 
-
-print(find_id_code(""))  # -> "Not enough numbers!"
-print(find_id_code("123456789123456789"))  # -> "Too many numbers!"
-print(find_id_code("ID code is: 49403136526"))  # -> "49403136526"
-print(find_id_code("efs4  9   #4aw0h 3r 1a36g5j2!!6-"))  # -> "49403136526"
+print(find_id_code(""))
+print(find_id_code("123456789123456789"))
+print(find_id_code("ID code is: 49403136526"))
+print(find_id_code("efs4  9   #4aw0h 3r 1a36g5j2!!6-"))
