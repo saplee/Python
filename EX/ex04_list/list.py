bd955361625d6722a495bc98d700074b9e23d9f7
Sander Pleesi <saplee@ttu.ee>
2021-09-20 00:13:10 +0300

    v4

---
+++ EX/ex04_list/list.py
@@ -0,0 +1,4 @@
+def generate_list(amount: int, data_type: str) -> list:
+    """Write a function that returns a list with amount elements of type data_type."""
+    list=[data_type]
+    return list*amount
