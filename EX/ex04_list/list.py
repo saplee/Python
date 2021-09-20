fd5d892bb23ee2738200d0c3e52de6dc09a2ee1a
Sander Pleesi <saplee@ttu.ee>
2021-09-20 17:06:12 +0300

    v4

--- EX/ex04_list/list.py
+++ EX/ex04_list/list.py
@@ -1,4 +1,22 @@
 def generate_list(amount: int, data_type: str) -> list:
     """Write a function that returns a list with amount elements of type data_type."""
-    list=[data_type]
-    return list*amount
+
+    if data_type=="int":
+        return [3]*amount
+    if data_type=="string":
+        return ["kass"]*amount
+    if data_type=="set":
+        return [set()]*amount
+    if data_type=="list":
+        return [1,2,3]*amount
+
+
+if __name__ == '__main__':
+    # The given outputs are only some of possible outputs, for example for (3, 'string') in the first part an output of ["kass", "koer", "kana"] would also work.
+
+    # Part 1
+    print(generate_list(2, 'set'))  # [set(), set()]
+    print(generate_list(3, 'string'))  # ["a", "kass", "a"]
+    print(generate_list(1, 'list'))  # [[]]
+    print(generate_list(5, 'int'))  # [1, 2, 3, 3, 3]
+    print()
