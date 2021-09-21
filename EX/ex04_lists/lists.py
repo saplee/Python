25a4b00a14b951d25d3935b07a065dce02f5df11
Sander Pleesi <saplee@ttu.ee>
2021-09-22 00:17:39 +0300

    v4

--- EX/ex04_lists/lists.py
+++ EX/ex04_lists/lists.py
@@ -37,7 +37,7 @@ def generate_combined_list_unique(inputs: list) -> list:
     """
     import random
     book = {"string": 0, "int": 0, "float": 0}
-    word=["a", "b", "c", "d", "f", "g", "h"]
+    word = ["a", "b", "c", "d", "f", "g", "h"]
     result = []
     for element in inputs:
         amount = element[0]
@@ -45,17 +45,15 @@ def generate_combined_list_unique(inputs: list) -> list:
         if book[data_type] < amount:
             book[data_type] = amount
     for i in range(0, book["int"]):
-            int_number = random.randint(1, 1000)
-            result += [int_number]
-            for i in range(0, book["float"]):
-                float_number = random.uniform(1, 100)
-                result += [float_number]
-            for i in range(0,book["string"]):
-                random_string=random.choice(word)
-                result+=[random_string]
+        int_number = random.randint(1, 1000)
+        result += [int_number]
+        for i in range(0, book["float"]):
+            float_number = random.uniform(1, 100)
+            result += [float_number]
+        for i in range(0, book["string"]):
+            random_string = random.choice(word)
+            result += [random_string]
     return result
-
-
 
 
 if __name__ == '__main__':
