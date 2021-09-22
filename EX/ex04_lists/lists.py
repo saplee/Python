ba44e828614c4a261e479091d54feaa9f552c488
Sander Pleesi <saplee@ttu.ee>
2021-09-22 18:46:36 +0300

    v4

--- EX/ex04_lists/lists.py
+++ EX/ex04_lists/lists.py
@@ -119,6 +119,5 @@ if __name__ == '__main__':
     # Part 4
     print(generate_combined_list_unique_advanced([(3, 'int'), (5, 'int')]))  # [1, 2, 3, 4, 5]
     print(generate_combined_list_unique_advanced([(2, 'list'), (3, 'string')]))  # ["a", [2], "asd", [], "abc"]
-    print(
-        generate_combined_list_unique_advanced([(2, 'float'), (3, 'dict')]))  # [{3: "abd"}, {"a": "a"}, {}, 3.14, 3.15]
+    print(generate_combined_list_unique_advanced([(2, 'float'), (3, 'dict')]))  # [{3: "abd"}, {"a": "a"}, {}, 3.14, 3.15]
     print()
