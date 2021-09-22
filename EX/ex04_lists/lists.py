a8299e9c7b5658591dff1f08709af8e6c63e1fc5
Sander Pleesi <saplee@ttu.ee>
2021-09-22 17:52:33 +0300

    v4

--- EX/ex04_lists/lists.py
+++ EX/ex04_lists/lists.py
@@ -1,4 +1,5 @@
 """"Lists."""
+import random
 
 
 def generate_list(amount: int, data_type: str) -> list:
@@ -12,7 +13,6 @@ def generate_list(amount: int, data_type: str) -> list:
 def generate_combined_list(inputs: list) -> list:
     """
     Write a function that returns a list with the minimal possible length, that still satisfies the criteria below.
-
     Every element of 'inputs' is a tuple (int amount, string data_type).
     For each element of 'inputs', it must be true that the returned list contains at least 'amount' of elements of type 'data_type'.
     """
@@ -38,7 +38,6 @@ def generate_combined_list_unique(inputs: list) -> list:
     Data types used in this function are 'int', 'float' and 'str' (string).
     The returned list can contain only unique elements.
     """
-    import random
     book = {"string": 0, "int": 0, "float": 0}
     result = []
     for element in inputs:
@@ -56,6 +55,39 @@ def generate_combined_list_unique(inputs: list) -> list:
         random_number = random.randint(0, 10000000)
         random_string = str(random_number)
         result += [random_string]
+    return result
+
+
+def generate_combined_list_unique_advanced(inputs: list) -> list:
+    """
+    Write a function that returns a list with the minimal possible length, that still satisfies the criteria below.
+
+    Every element of 'inputs' is a tuple (int amount, string data_type).
+    For each element of 'inputs', it must be true that the returned list contains at least 'amount' of elements of type 'data_type'.
+    All the data types from the first function are used here.
+    The returned list can contain only unique elements.
+    """
+    book = {"string": 0, "int": 0, "set": 0, "list": 0, "dict": 0, "float": 0}
+    result = []
+    for element in inputs:
+        amount = element[0]
+        data_type = element[1]
+        if book[data_type] < amount:
+            book[data_type] = amount
+    for i in range(0, book["int"]):
+        int_number = random.randint(0, 100000000)
+        result += [int_number]
+    for i in range(0, book["float"]):
+        float_number = random.uniform(0, 10000000)
+        result += [float_number]
+    for i in range(0, book["string"]):
+        random_number = random.randint(0, 10000000)
+        random_string = str(random_number)
+        result += [random_string]
+    for l in range(0, book["list"]):
+        random_numbers=random.randint(0, 10000000)
+        random_list=[random_numbers]
+        result+=[random_list]
     return result
 
 
@@ -79,4 +111,9 @@ if __name__ == '__main__':
     # Part 3
     print(generate_combined_list_unique([(3, 'int'), (5, 'int')]))  # [1, 2, 3, 4, 5]
     print(generate_combined_list_unique([(2, 'int'), (2, 'float'), (1, 'int')]))  # [43, 93, 4.3, 2.1]
+    print()
+    # Part 4
+    print(generate_combined_list_unique_advanced([(3, 'int'), (5, 'int')]))  # [1, 2, 3, 4, 5]
+    print(generate_combined_list_unique_advanced([(2, 'list'), (3, 'string')]))  # ["a", [2], "asd", [], "abc"]
+    print(generate_combined_list_unique_advanced([(2, 'float'), (3, 'dict')]))  # [{3: "abd"}, {"a": "a"}, {}, 3.14, 3.15]
     print()
