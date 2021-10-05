0d48e5822d4c44ba2735be17f8395c958a7a0019
Sander Pleesi <saplee@ttu.ee>
2021-10-05 14:28:25 +0300

    v4

--- EX/ex06_regex/easy.py
+++ EX/ex06_regex/easy.py
@@ -1,4 +1,4 @@
-"""Regex, yay!"""
+"""Regex, yay"""
 import re
 
 
@@ -173,4 +173,5 @@ if __name__ == '__main__':
     print(find_words_from_sentences_only(
         'See on esimene - ä lause. See, on teine: lause! see ei ole lause. Aga kas see on? jah, oli.'))  # ['See', 'on', 'esimene', 'ä', 'lause', 'See', 'on', 'teine', 'lause', 'Aga', 'kas', 'see', 'on']
     print(find_years("1998sef672387fh3f87fh83777f777f7777f73wfj893w8938434343"))  # [1998, 7777]
-    print(find_phone_numbers("+372 56887364  +37256887364  +33359835647  56887364"))  # {'+372': ['56887364', '56887364'], '+333': ['59835647'], '': ['56887364']}
+    print(find_phone_numbers(
+        "+372 56887364  +37256887364  +33359835647  56887364"))  # {'+372': ['56887364', '56887364'], '+333': ['59835647'], '': ['56887364']}
