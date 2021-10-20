3f067fc5d5602767dd6122959009d57558ae77f1
Sander Pleesi <saplee@ttu.ee>
2021-10-20 16:53:27 +0300

    v4

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -2,11 +2,23 @@ import solution
 
 
 def test_part1():
-    for number in range(18-26):
+    for number in range(18, 26):
         first_result = solution.students_study(number, False)
         result = True
         assert first_result == result
-    for number2 in range(1-5):
-        res = solution.students_study(number2)
+    for number in range(18, 26):
+        first_result = solution.students_study(number, True)
+        result = True
+        assert first_result == result
+    for number2 in range(1, 5):
+        res = solution.students_study(number2, False)
         result = False
+        assert res == result
+    for number3 in range(1, 5):
+        res = solution.students_study(number3, True)
+        result = False
+        assert res == result
+    for number4 in range(5, 18):
+        res = solution.students_study(number4, True)
+        result = True
         assert res == result
