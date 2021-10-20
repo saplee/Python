7334fd7e9fb7d19f0e4ca84e0fb68b46d05998f0
Sander Pleesi <saplee@ttu.ee>
2021-10-20 12:54:37 +0300

    v4

--- EX/ex08_tests/test.py
+++ EX/ex08_tests/test.py
@@ -17,9 +17,9 @@ def test_part2():
 
 
 def test_part3():
-    input_amount = 1
+    input_amount = 0
     res = solution.generate_list(input_amount, "float")
-    expected_len = 1
+    expected_len = 0
     assert len(res) == expected_len
 
 
@@ -44,7 +44,20 @@ def test_part6():
     assert len(res) == expected_len
 
 
-def test_part1():
+def test_part1_second():
     res = solution.generate_combined_list([(3, "int"), (2, "dict")])
     result = 5
     assert len(res) == result
+
+
+def test_part2_second():
+    res = solution.generate_combined_list([])
+    result = 0
+    assert len(res) == result
+
+
+def test_part3_second():
+    res = solution.generate_combined_list([(3, "int"), (5, "int"), (4, "int")])
+    result = 5
+    assert len(res) == result
+
