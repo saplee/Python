e7f67a386b0ab85a16253e4a067327e3f43a84fc
Sander Pleesi <saplee@ttu.ee>
2021-10-20 18:01:10 +0300

    v4

--- EX/ex08_solution_and_tests/solution.py
+++ EX/ex08_solution_and_tests/solution.py
@@ -18,3 +18,14 @@ def lottery(a: int, b: int, c: int) -> int:
         return 1
     if b == a or c == a:
         return 0
+
+
+def fruit_order(small_baskets: int, big_baskets: int, ordered_amount: int) -> int:
+    """Return number of small fruit baskets if it's possible to finish the order, otherwise return -1."""
+    small_sum = small_baskets
+    big_sum = big_baskets * 5
+    result = big_sum + small_sum
+    if result == ordered_amount:
+        return small_sum
+    else:
+        return -1
