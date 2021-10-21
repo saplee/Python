ceee7ba4c0dc47be70c4e1690efb77b7aec0dba8
Sander Pleesi <saplee@ttu.ee>
2021-10-21 18:01:22 +0300

    v4

--- EX/ex08_solution_and_tests/solution.py
+++ EX/ex08_solution_and_tests/solution.py
@@ -1,3 +1,6 @@
+"""Ex08 solution"""
+
+
 def students_study(time: int, coffee_needed: bool) -> bool:
     """Return True if students study in given circumstances."""
     if 5 <= time <= 17 and coffee_needed is True:
