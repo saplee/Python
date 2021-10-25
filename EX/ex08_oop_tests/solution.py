5648ca21989e197ff38be2ffeddf38f9096e06fa
Sander Pleesi <saplee@ttu.ee>
2021-10-25 18:51:14 +0300

    v4

--- EX/ex08_oop_tests/solution.py
+++ EX/ex08_oop_tests/solution.py
@@ -3,10 +3,17 @@ class Factory:
         self.basic = 0
         self.medium = 0
         self.large = 0
+        self.amount = 0
 
     def bake_cake(self, toppings: int, base: int) -> int:
-        amount=0
-
+        amount = 0
+        if toppings == base:
+            self.large = base // 5
+            self.medium = (base % 5) // 2
+            self.basic = ((base % 5) % 2) // 1
+            amount = (base // 5) + ((base % 5) // 2) + (((base % 5) % 2) // 1)
+            self.amount = amount
+        return amount
 
     def get_last_cakes(self, n: int) -> list:
         pass
@@ -21,14 +28,22 @@ class Factory:
 class Cake:
 
     def __init__(self, base_amount, toppings_amount):
-        pass
+        self.base_amount = base_amount
+        self.toppings_amount = toppings_amount
+        self.cake_size = ""
 
     @property
     def type(self):
-        pass
+        if self.base_amount == 1 and self.toppings_amount == 1:
+            self.cake_size = "basic"
+        if self.base_amount == 2 and self.toppings_amount == 2:
+            self.cake_size = "medium"
+        if self.base_amount == 5 and self.toppings_amount == 5:
+            self.cake_size = "large"
+        return self.cake_size
 
     def __repr__(self):
-        pass
+        return self.type
 
     def __eq__(self, other):
         pass
@@ -36,4 +51,3 @@ class Cake:
 
 class WrongIngredientsAmountException(Exception):
     pass
-
