2bba2146661959cd3956943c5a0f42d3b9ff27e8
Sander Pleesi <saplee@ttu.ee>
2021-10-25 21:57:53 +0300

    v4

--- KT/kt0/exam.py
+++ KT/kt0/exam.py
@@ -52,11 +52,14 @@ def nr_into_num_list(nr: int, num_list: list) -> list:
     nr_into_num_list(5, [1,2,3,4,5,6]) -> [1,2,3,4,5,5,6]
     nr_into_num_list(0, [1,2,3,4,5]) -> [0,1,2,3,4,5,]
     """
-    if not num_list or nr > num_list[-1]:
+    if not num_list:
         num_list.append(nr)
         return num_list
     if nr <= num_list[0]:
         num_list.insert(0, nr)
+        return num_list
+    if nr > num_list[-1]:
+        num_list.append(nr)
         return num_list
     else:
         for number in num_list:
