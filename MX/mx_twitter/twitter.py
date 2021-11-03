8c6c9e580f17bc748c39feaf3274c19e617e0295
Sander Pleesi <saplee@ttu.ee>
2021-11-03 14:14:40 +0200

    v4

--- MX/mx_twitter/twitter.py
+++ MX/mx_twitter/twitter.py
@@ -92,6 +92,7 @@ def sort_hashtags_by_popularity(tweets: list) -> list:
     :return: List of hashtags by popularity.
     """
     my_dict = {}
+    result = []
     for tweet in tweets:
         match = re.findall(r'#\w+', tweet.content)
         if len(match) != 0 and match[0] not in my_dict:
@@ -99,14 +100,16 @@ def sort_hashtags_by_popularity(tweets: list) -> list:
         elif len(match) != 0:
             my_dict[match[0]] = my_dict[match[0]] + tweet.retweets
     new_dicts = dict(sorted(my_dict.items(), key=lambda item: item[0]))
-    new_list = sorted(new_dicts, key=lambda x: new_dicts[x], reverse=True)
-    return new_list
+    new_dict = dict(sorted(new_dicts.items(), key=lambda item: item[1], reverse=True))
+    for key in new_dict.keys():
+        result.append(key)
+    return result
 
 
 if __name__ == '__main__':
     tweet1 = Tweet("@realDonaldTrump", "Despite the negative press covfefe #bigsmart", 1249, 54303)
-    tweet2 = Tweet("@elonmusk", "Technically, alcohol is a solution #bigsmart", 366.4, 1166500)
-    tweet3 = Tweet("@CIA", "We can neither confirm nor deny that this is our first tweet. #aeart", 2192, 284200)
+    tweet2 = Tweet("@elonmusk", "Technically, alcohol is a solution #bigsmart", 366.4, 166500)
+    tweet3 = Tweet("@CIA", "We can neither confirm nor deny that this is our first tweet. #heart", 2192, 284200)
     tweets = [tweet1, tweet2, tweet3]
 
     print(find_fastest_growing(tweets).user)  # -> "@elonmusk"
