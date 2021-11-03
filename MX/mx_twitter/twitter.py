19055d2a5667f98988707dd0ee97389b098983b5
Sander Pleesi <saplee@ttu.ee>
2021-11-03 16:46:59 +0200

    v4

--- MX/mx_twitter/twitter.py
+++ MX/mx_twitter/twitter.py
@@ -98,7 +98,7 @@ def sort_hashtags_by_popularity(tweets: list) -> list:
         if len(match) != 0 and match[0] not in my_dict:
             my_dict[match[0]] = tweet.retweets
         elif len(match) != 0:
-            my_dict[match[0]] += tweet.retweets
+            my_dict[match[0]] = my_dict[match[0]] + tweet.retweets
     new_dicts = dict(sorted(my_dict.items(), key=lambda item: item[0]))
     new_dict = dict(sorted(new_dicts.items(), key=lambda item: item[1], reverse=True))
     for key in new_dict.keys():
@@ -109,7 +109,7 @@ def sort_hashtags_by_popularity(tweets: list) -> list:
 if __name__ == '__main__':
     tweet1 = Tweet("@realDonaldTrump", "Despite the negative press covfefe #bigsmart", 1249, 54303)
     tweet2 = Tweet("@elonmusk", "Technically, alcohol is a solution #bigsmart", 366.4, 166500)
-    tweet3 = Tweet("@CIA", "We can neither confirm nor deny that this is our first tweet. #aeart", 2192, 284200)
+    tweet3 = Tweet("@CIA", "We can neither confirm nor deny that this is our first tweet. #heart", 2192, 284200)
     tweets = [tweet1, tweet2, tweet3]
 
     print(find_fastest_growing(tweets).user)  # -> "@elonmusk"
