475092587335b875baca6905d3c5c33f29844e74
Sander Pleesi <saplee@ttu.ee>
2021-11-10 17:47:23 +0200

    v4

--- EX/ex11_order/order.py
+++ EX/ex11_order/order.py
@@ -147,12 +147,12 @@ class ContainerAggregator:
         """
         result = {}
         for order in orders:
-            new_container_volume = self.container_volume
             my_list = [order]
             if order.total_volume > self.container_volume:
                 self.not_used_orders.append(order)
             if order.total_volume <= self.container_volume and order.destination not in result:
                 result[order.destination] = [Container(self.container_volume, my_list)]
+                new_container_volume = self.container_volume
                 new_container_volume -= order.total_volume
             elif order.destination in result and new_container_volume >= order.total_volume:
                 my_list.append(order)
