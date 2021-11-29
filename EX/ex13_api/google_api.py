a4095f7540b97c6d26c3e8ff8a910ff7977598b4
Sander Pleesi <saplee@ttu.ee>
2021-11-29 14:25:55 +0200

    v4

---
+++ EX/ex13_api/google_api.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+
+def get_links_from_spreadsheet(id: str, token: str) -> list:
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    SAMPLE_RANGE_NAME = 'A:A'
+    my_list = []
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(token):
+        creds = Credentials.from_authorized_user_file(token, SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(token, 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=id,
+                                range=SAMPLE_RANGE_NAME).execute()
+    values = result.get('values', [])
+    for value in values:
+        for item in value:
+            my_list.append(item)
+    return result
