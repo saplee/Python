4c0c86222e81cf220123a34400ef9e5f21596287
Sander Pleesi <saplee@ttu.ee>
2021-11-30 13:51:25 +0200

    v4

--- EX/ex13_api/google_api.py
+++ EX/ex13_api/google_api.py
@@ -46,13 +46,7 @@ def get_links_from_spreadsheet(id: str, token: str) -> list:
 
 
 def get_links_from_playlist(link: str, developer_key: str) -> list:
-    """Return a list of links to songs in the Youtube playlist with the given address.Example input
-        get_links_from_playlist('https://www.youtube.com/playlist?list=PLFt_AvWsXl0ehjAfLFsp1PGaatzAwo0uK',
-                                'ThisIsNotARealKey_ThisIsNotARealKey')
-
-    Returns
-        ['https://youtube.com/watch?v=r_It_X7v-1E', 'https://youtube.com/watch?v=U4ogK0MIzqk', ... and so on]
-    """
+    """Return a list of links to songs in the Youtube playlist with the given address."""
     my_list = []
 
     api_service_name = "youtube"
