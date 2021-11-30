5906d5f6a966288383c94032f4f02ccd0ccf09d1
Sander Pleesi <saplee@ttu.ee>
2021-11-30 13:19:21 +0200

    v4

--- EX/ex13_api/google_api.py
+++ EX/ex13_api/google_api.py
@@ -5,6 +5,7 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+import re
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
@@ -13,7 +14,6 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 def get_links_from_spreadsheet(id: str, token: str) -> list:
     """Show basic usage of the Sheets API.Prints values from a sample spreadsheet."""
     SAMPLE_RANGE_NAME = 'A:A'
-    SAMPLE_SPREADSHEET_ID = id
     my_list = []
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
@@ -34,10 +34,48 @@ def get_links_from_spreadsheet(id: str, token: str) -> list:
 
     service = build('sheets', 'v4', credentials=creds)
     sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+    result = sheet.values().get(spreadsheetId=id,
                                 range=SAMPLE_RANGE_NAME).execute()
     values = result.get('values', [])
     for value in values:
         for item in value:
             my_list.append(item)
     return my_list
+
+
+def get_links_from_playlist(link: str, developer_key: str) -> list:
+    """
+    Return a list of links to songs in the Youtube playlist with the given address.
+    Example input
+        get_links_from_playlist('https://www.youtube.com/playlist?list=PLFt_AvWsXl0ehjAfLFsp1PGaatzAwo0uK',
+                                'ThisIsNotARealKey_____ThisIsNotARealKey')
+
+    Returns
+        ['https://youtube.com/watch?v=r_It_X7v-1E', 'https://youtube.com/watch?v=U4ogK0MIzqk', ... and so on]
+    """
+    import googleapiclient.discovery
+    import googleapiclient.errors
+    result = []
+    api_service_name = "youtube"
+    api_version = "v3"
+    match = re.search(r'=(\w+)', link)
+    playlist_id = match.group(1)
+    # Get credentials and create an API client
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey=developer_key)
+
+    request = youtube.playlistItems().list(
+        part="contentDetails",
+        maxResults=25,
+        playlistId=playlist_id
+    )
+    response = request.execute()
+    for key in response:
+        if key == "items":
+            for item in response[key]:
+                for items in item:
+                    if items == "contentDetails":
+                        for value in key[items]:
+                            if value == "videoId":
+                                result.append(f'https://youtube.com/watch?v={key[items][value]}')
+    return result
