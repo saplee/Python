124f067c628d1f52a2516eeb8b5cc4ef20ccd83f
Sander Pleesi <saplee@ttu.ee>
2021-11-30 13:49:25 +0200

    v4

--- EX/ex13_api/google_api.py
+++ EX/ex13_api/google_api.py
@@ -46,36 +46,33 @@ def get_links_from_spreadsheet(id: str, token: str) -> list:
 
 
 def get_links_from_playlist(link: str, developer_key: str) -> list:
-    """
-    Return a list of links to songs in the Youtube playlist with the given address.
-    Example input
+    """Return a list of links to songs in the Youtube playlist with the given address.Example input
         get_links_from_playlist('https://www.youtube.com/playlist?list=PLFt_AvWsXl0ehjAfLFsp1PGaatzAwo0uK',
-                                'ThisIsNotARealKey_____ThisIsNotARealKey')
+                                'ThisIsNotARealKey_ThisIsNotARealKey')
 
     Returns
         ['https://youtube.com/watch?v=r_It_X7v-1E', 'https://youtube.com/watch?v=U4ogK0MIzqk', ... and so on]
     """
-    result = []
+    my_list = []
+
     api_service_name = "youtube"
     api_version = "v3"
-    match = re.search(r'=(\w+)', link)
-    playlist_id = match.group(1)
-    # Get credentials and create an API client
+
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey=developer_key)
 
     request = youtube.playlistItems().list(
         part="contentDetails",
         maxResults=25,
-        playlistId=playlist_id
+        playlistId=re.search(r'=(\w+)', link).group(1)
     )
     response = request.execute()
     for key in response:
-        if key == "items":
+        if key == 'items':
             for item in response[key]:
-                for items in item:
-                    if items == "contentDetails":
-                        for value in item[items]:
-                            if value == "videoId":
-                                result.append(f'https://youtube.com/watch?v={key[items][value]}')
-    return result
+                for new_item in item:
+                    if new_item == 'contentDetails':
+                        for value in item[new_item]:
+                            if value == 'videoId':
+                                my_list.append(f'https://youtube.com/watch?v={item[new_item][value]}')
+    return my_list
