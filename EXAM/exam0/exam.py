ba698618d979ef518a78f3820631e85cae7df0f8
Sander Pleesi <saplee@ttu.ee>
2021-12-09 23:04:38 +0200

    v4

--- EXAM/exam0/exam.py
+++ EXAM/exam0/exam.py
@@ -218,7 +218,7 @@ def get_top_student_with_credit_points(students: list, min_credit_points: int):
         if name.credit_points >= min_credit_points and name.credit_points > min_number:
             min_number = name.credit_points
             student.append(name)
-    return student[0]
+    return student
 
 
 def add_result_to_student(student: Student, grades_count: int, new_grade: int, credit_points) -> Student:
