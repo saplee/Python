d9a2bf07978d46c47a321d656f4450f3d6859b9d
Sander Pleesi <saplee@ttu.ee>
2021-12-09 13:24:30 +0200

    v4

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -59,7 +59,7 @@ def follow_the_line(robot: FollowerBot):
     :param FollowerBot robot: instance of the robot that you need to make move
     """
     robot.set_wheels_speed(100)
-    robot.sleep(0.15)
+    robot.sleep(0.1)
     while True:
         sensor_2 = robot.get_second_line_sensor_from_left()
         sensor_1 = robot.get_third_line_sensor_from_left()
@@ -71,8 +71,8 @@ def follow_the_line(robot: FollowerBot):
         if sensor_2 >= 700 and (sensor_4 < 200 or sensor_5 < 200 or sensor_6 < 200):
             while True:
                 print("world")
-                robot.set_right_wheel_speed(55)
-                robot.set_left_wheel_speed(35)
+                robot.set_right_wheel_speed(65)
+                robot.set_left_wheel_speed(45)
                 new_sensor_5 = robot.get_second_line_sensor_from_right()
                 robot.sleep(0.01)
                 if new_sensor_5 >= 700:
@@ -80,8 +80,8 @@ def follow_the_line(robot: FollowerBot):
                     break
         elif sensor_5 >= 700 and (sensor_3 < 200 or sensor_2 < 200 or sensor_1 < 200):
             while True:
-                robot.set_right_wheel_speed(35)
-                robot.set_left_wheel_speed(55)
+                robot.set_right_wheel_speed(45)
+                robot.set_left_wheel_speed(65)
                 new_sensor_2 = robot.get_second_line_sensor_from_left()
                 robot.sleep(0.01)
                 if new_sensor_2 >= 700:
