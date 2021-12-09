2c30f6161ab95c866a30ef7907873bf7187a30fa
Sander Pleesi <saplee@ttu.ee>
2021-12-09 12:04:09 +0200

    v4

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -69,10 +69,10 @@ def follow_the_line(robot: FollowerBot):
                 if new_sensor_2 >= 700:
                     robot.set_wheels_speed(0)
                     break
-        elif sensor_3 < 200 and sensor_2 < 200:
+        elif sensor_3 < 200 and sensor_4 < 200:
             while True:
-                robot.set_right_wheel_speed(20)
-                robot.set_left_wheel_speed(20)
+                robot.set_right_wheel_speed(25)
+                robot.set_left_wheel_speed(25)
                 new_sensor_5 = robot.get_second_line_sensor_from_right()
                 new_sensor_2 = robot.get_second_line_sensor_from_left()
                 robot.sleep(0.01)
