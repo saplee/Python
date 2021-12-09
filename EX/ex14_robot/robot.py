8df330c9fa1f4623ad0cc8df327d08f47a46c817
Sander Pleesi <saplee@ttu.ee>
2021-12-09 13:07:07 +0200

    v4

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -57,8 +57,8 @@ def follow_the_line(robot: FollowerBot):
 
     :param FollowerBot robot: instance of the robot that you need to make move
     """
-    robot.set_wheels_speed(20)
-    robot.sleep(1)
+    robot.set_wheels_speed(100)
+    robot.sleep(0.4)
     while True:
         sensor_2 = robot.get_second_line_sensor_from_left()
         sensor_1 = robot.get_third_line_sensor_from_left()
